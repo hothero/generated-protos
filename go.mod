2cc22fec7e3787fdcfaec3017ab4744995ff37af
Ronald Hsu (hothero) <hothero0705@gmail.com>
2022-10-12 00:00:17 +0800

    import dependency

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/hothero/generated-protos
 
 go 1.16
+
+require github.com/shopspring/decimal v1.3.1
--- listing/types.go
+++ listing/types.go
@@ -1,5 +1,9 @@
 package listing
 
+import "github.com/shopspring/decimal"
+
 type Listing struct {
+	ID    string
 	Title string
+	Price decimal.Decimal
 }
